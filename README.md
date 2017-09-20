0533902125a6d13433f3ec3008c31b197e8c55a2
bsweger <bsweger@gmail.com>
2017-09-19 23:14:24 -0400

    Add data loading code, .csv, sample config and update README

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# data_loading
-Quick script for using pandas to load a .csv file to postgres
+# Data Loading
+Simple Python script for using pandas to load a .csv file to postgres.
+
+This is script is for reference/proof of concept and is not production-ready. At a high level it:
+
+1. Reads a sample .csv into a pandas dataframe.
+2. Does some data cleaning.
+3. Bulk inserts the clean data into a postgres table.
+
+## Setup
+
+1. Make sure you have access to a Postgres database and credentials that can create tables and insert records.
+2. Create a Python virtual environment and activate it.
+3. Clone this repo and `cd` into the `data_loading` folder.
+4. Install the required Python libraries: `pip install -r requirements.txt`.
+5. Rename (or copy) `example_db.cfg` to `db.cfg` and update the information with your postgres hostname, dbname, credentials, and the name of the target table you want load.
---
+++ load_csv.py
@@ -0,0 +1,76 @@
+import configparser
+import logging
+
+import numpy as np
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+def load_csv(file_location):
+    """Load a .csv into a pre-existing postgres table."""
+
+    # TODO: more robust logging setup and execution
+    logging.basicConfig(level=logging.INFO)
+
+    # get db credentials and connection info from config file
+    config = configparser.ConfigParser()
+    config.read('db.cfg')
+    postgres = config['postgres']
+    target_table = postgres['table']
+
+    # create database engine and connection
+    uri = get_postgres_uri(postgres)
+    engine = create_engine(uri)
+    connection = engine.connect()
+
+    data = pd.read_csv(file_location, encoding='latin_1')
+    logging.info('Read {} rows from {}'.format(len(data), file_location))
+    data = clean_data(data)
+
+    # bulk insert data to table (drops and re-creates table first)
+    data.to_sql(target_table, connection, if_exists='replace', index=False)
+
+    # check rowcount of the target table
+    # TODO: raise error if db rowcount doesn't match number of dataframe rows
+    sql = 'select count(*) from {}'.format(target_table)
+    db_rows = engine.execute(sql).scalar()
+    logging.info('Inserted {} rows to database'.format(db_rows))
+
+
+def clean_data(data):
+    """Return a clean copy of the dataset."""
+
+    # Update 'null' strings' to empty values in the lang column
+    data.lang.replace(r'(?i)null', np.nan, inplace=True, regex=True)
+
+    # Update 'null' strings to empty values in the date column
+    data.datecreated.replace(r'(?i)null', np.nan, inplace=True, regex=True)
+
+    # Remove duplicate id (keep the first one)
+    original_rowcount = len(data)
+    data.drop_duplicates(subset='id', inplace=True)
+    logging.info('Removed {} duplicate ids from data'.format(
+        original_rowcount - len(data)))
+
+    return data
+
+
+def get_postgres_uri(postgres):
+    """Returns a postgres connection URI."""
+
+    # TODO: add some error checking of config file values
+    # TODO: consider setting an environment variable with the uri string instead
+    user = postgres.get('user')
+    password = postgres.get('password')
+    host = postgres.get('host', 'localhost')
+    port = postgres.get('port', 5432)
+    dbname = postgres.get('dbname', 'postgres')
+
+    return 'postgresql://{}:{}@{}:{}/{}'.format(
+        user, password, host, port, dbname
+    )
+
+
+if __name__ == '__main__':
+    # TODO: don't hardcode the filename
+    load_csv('data0.csv')
